96d6356e415e8b2ab99583bb20a3c5798609109d
Ivchenko Artem <metrash.a70@gmail.com>
2022-07-01 20:31:47 +0300

    Task number 5 part 1

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from './Header.module.css'
 
 function Header() {
+    const classActive = ({isActive}: any) => isActive ? s.active : '';
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.container}>
+            {/*// add NavLinks*/}
+            <NavLink className={`${classActive} ${s.item1}`} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
+            <NavLink className={`${classActive} ${s.item2}`} to={PATH.Junior}>Junior</NavLink>
+            <NavLink className={`${classActive} ${s.item3}`} to={PATH.Junior_Plus}>Junior Plus</NavLink>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -4,8 +4,7 @@ function Error404() {
     return (
         <div>
             <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <div>Ненавижу css</div>
         </div>
     )
 }
