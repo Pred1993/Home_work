a79e0221f1c1d3f270eeea5e323b47ff16dd00c1
Ivchenko Artem <metrash.a70@gmail.com>
2022-07-02 11:46:19 +0300

    Task number 5 part 2

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -4,13 +4,13 @@ import {PATH} from "./Pages";
 import s from './Header.module.css'
 
 function Header() {
-    const classActive = ({isActive}: any) => isActive ? s.active : '';
+    const classActive = ({isActive}: any) => isActive ? `${s.active}` : s.item
     return (
         <div className={s.container}>
             {/*// add NavLinks*/}
-            <NavLink className={`${classActive} ${s.item1}`} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
-            <NavLink className={`${classActive} ${s.item2}`} to={PATH.Junior}>Junior</NavLink>
-            <NavLink className={`${classActive} ${s.item3}`} to={PATH.Junior_Plus}>Junior Plus</NavLink>
+            <NavLink className={classActive} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
+            <NavLink className={classActive} to={PATH.Junior}>Junior</NavLink>
+            <NavLink className={classActive} to={PATH.Junior_Plus}>Junior Plus</NavLink>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import s from './Error404.module.css'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Ненавижу css</div>
+        <div className={s.screen}>
+            <div className={s.container}>
+                <h1 className={s.error}><span>Error - 404</span></h1>
+                <p className={s.text}>To solve the error take the following measures:</p>
+                <ul className={s.ul}>
+                    <li>Check your network connection</li>
+                    <li>Enter the correct address</li>
+                    <li>Try to come back later</li>
+                </ul>
+            </div>
         </div>
     )
 }
